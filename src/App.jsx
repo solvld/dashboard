a91f0a89929c36cd6107445f63d5eb1e0826254a
solvld <sol.vld@yandex.ru>
2024-01-03 20:27:47 +0400

    create app template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import LeftColumn from "./Components/LeftColumn"
+import NavBar from "./Components/NavBar"
+import RightColumn from "./Components/RightColumn"
+import SideBar from "./Components/SideBar"
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
+		<main className='flex'>
+			<SideBar />
+			<div className='flex flex-col flex-1 relative'>
+				<NavBar />
+
+				<div className='grid md:grid-cols-3 grid-cols-1 w-full'>
+					<div className="col-span-2">
+            <LeftColumn />
+          </div>
+					<div className="w-full">
+            <RightColumn />
+          </div>
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default App
