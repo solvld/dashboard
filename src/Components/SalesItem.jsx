dc3475a63aefd7e85a9603c328ca3e5faec6b333
solvld <sol.vld@yandex.ru>
2024-01-05 23:12:49 +0400

    add SalesItem component

--- src/Components/SalesItem.jsx
+++ src/Components/SalesItem.jsx
@@ -1,5 +1,5 @@
-import { Card, Flex, Tab, TabGroup, TabList, Title, Text, Metric, Divider, Bold, DonutChart } from '@tremor/react'
-import { ChartPieIcon, ListBulletIcon } from '@heroicons/react/24/outline'
+import { Card, Flex, Tab, TabGroup, TabList, Title, Text, Metric, Divider, Bold, DonutChart, List, ListItem, Button } from '@tremor/react'
+import { ArrowRightIcon, ChartPieIcon, ListBulletIcon } from '@heroicons/react/24/outline'
 import React, { useState } from 'react'
 
 const cities = [
@@ -51,7 +51,7 @@ function SalesItem() {
 			<Text className='mt-8'>
 				<Bold>Asset Allocation</Bold>
 			</Text>
-      <Text>1 Asset</Text>
+			<Text>1 Asset</Text>
 			{selectedIndex === 0 ? (
 				<DonutChart
 					className='mt-6'
@@ -62,8 +62,30 @@ function SalesItem() {
 				/>
 			) : (
 				<>
-          <Flex className='mt-8 justify-between'></Flex>
-        </>
+					<Flex className='mt-8' justifyContent='between'>
+						<Text className='truncate'>
+							<Bold>Stocks</Bold>
+						</Text>
+						<Text>Since Transaction</Text>
+					</Flex>
+					<List>
+						{cities.map(city => (
+							<ListItem key={city.name}>
+								<Text>{city.name}</Text>
+								<Flex className='space-x-2' justifyContent='end'>
+									<Text>{valueFormatter(city.sales)}</Text>
+								</Flex>
+							</ListItem>
+						))}
+					</List>
+					<Button
+						className='mt-6 border-b'
+						size='xs'
+						variant='light'
+					>
+						View More
+					</Button>
+				</>
 			)}
 		</Card>
 	)
